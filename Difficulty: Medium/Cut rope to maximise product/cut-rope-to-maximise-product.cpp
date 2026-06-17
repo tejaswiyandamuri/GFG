4218d3200136b9c48c872b9145273d65ff208d15
Tejaswi Yandamuri <tejaswiyandamuri06@gmail.com>
2026-06-17 10:33:03 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Cut rope to maximise product/cut-rope-to-maximise-product.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int maxProduct(int n) {
+        // code here
+        if (n == 2) return 1;
+
+        if (n == 3) return 2;
+
+        int ans = 1;
+
+        while (n > 4) {
+            ans *= 3;
+            n -= 3;
+        }
+
+        return ans * n;
+    }
+};
